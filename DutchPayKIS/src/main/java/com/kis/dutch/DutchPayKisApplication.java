d34b90777c6c2f93568b97a24f0673f236054d57
S.C KIM <ratm1026@gmail.com>
2018-01-31 09:21:22 +0900

    test

---
+++ DutchPayKIS/src/main/java/com/kis/dutch/DutchPayKisApplication.java
@@ -0,0 +1,12 @@
+package com.kis.dutch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchPayKisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchPayKisApplication.class, args);
+	}
+}
